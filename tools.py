8ec7220729ada3036595efa522aefbe090ae643d
EshaBok <eshabokhari@outlook.com>
2026-04-07 14:04:04 -0700

    Added a new file.
    Tools files essentially giving structure and mechanics to detect if an email is likely to be fraud. Determines if the email is fraudulent or safe by calculating a score.

---
+++ tools.py
@@ -0,0 +1,28 @@
+fraud_words = ["urgent", "action required", "free", "congradulations"]
+urgency_words = ["Now", "Urgency", "Deadline", "immediately", "fast", "act now", "hurry", "quick"]
+financial_words = ["money", "cash", "prize", "winner", "lottery", "inheritance", "account", "invoice", "transfer"]
+action_words = ["click here", "download", "open attachment", "provide information", "verify account", "update information", "verify attachment", "login"]
+
+def scan_email_for_fraud(email):
+   email = email.lower()
+   score = 0
+   for word in fraud_words:
+      if word in email:
+         score += 10
+   for word in urgency_words:
+      if word in email:
+         score += 5
+   for word in financial_words:
+      if word in email:
+         score += 8
+   for word in action_words:
+      if word in email:
+         score += 6
+   return score
+
+email = input("Copy and enter the email content: ")
+emailscore = scan_email_for_fraud(email)
+if emailscore >= 15:
+   print("This email is likely to be fraudulent.")
+else:   print("This email is likely to be safe.")
+
